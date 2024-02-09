8ad4034fd8af06886337064aa1f2ddf07d53f14c
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-02-10 00:14:58 +0800

    Update immo_diy.sh

--- immo_diy.sh
+++ immo_diy.sh
@@ -12,8 +12,10 @@ svn_export() {
 	rm -rf "$TMP_DIR"
 }
 
-#find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
+
+git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
