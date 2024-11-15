0a25b6cba5fdc49ac93867e8f9048ca4d4a905b8
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-11-15 23:20:24 +0800

    更新 test_diy.sh

--- test_diy.sh
+++ test_diy.sh
@@ -30,6 +30,7 @@ git clone --depth 1 https://github.com/sirpdboy/netspeedtest package/netspeedtes
 ./scripts/feeds install -a
 
 latest_ver=$(wget-ssl --no-check-certificate -t 2 -T 20 -O - https://api.github.com/repos/XGHeaven/homebox/releases/latest 2>/dev/null|grep -E 'tag_name'|head -n1|cut -d '"' -f4|sed 's/\./\\\./g' 2>/dev/null)
+echo $latest_ver
 sed -i "s/PKG_VERSION:=.*/PKG_VERSION:=$latest_ver/" package/netspeedtest/homebox/Makefile
 
 echo "*********"
