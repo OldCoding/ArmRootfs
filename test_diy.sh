bee2e09aaf5dae4eb1ace638be0b20799c707c07
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-11-27 19:09:52 +0800

    test

--- test_diy.sh
+++ test_diy.sh
@@ -13,15 +13,13 @@ svn_export() {
 }
 
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/packages/net/v2ray-plugin
-rm -rf feeds/packages/net/xray-core
-
 
 #git clone --depth 1 https://github.com/sirpdboy/netspeedtest package/netspeedtest
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 svn_export "main" "luci-app-passwall" "package/luci-app-passwall" "https://github.com/xiaorouji/openwrt-passwall"
 svn_export "main" "luci-app-passwall2" "package/luci-app-passwall2" "https://github.com/xiaorouji/openwrt-passwall2"
+
+rm -rf package/openwrt-passwall-packages/v2ray-geodata
 
 #latest_ver=$(curl -sfL https://api.github.com/repos/XGHeaven/homebox/releases/latest |grep -E 'tag_name'|head -n1|cut -d '"' -f4|sed 's/\./\\\./g')
 #sed -i "s/\$(PKG_VERSION)/${latest_ver:1}/" package/netspeedtest/homebox/Makefile
