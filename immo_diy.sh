30d8b05c0ad929ea9f558785eb55542f2c0337a8
OldCoding <qq1049066154@gmail.com>
2026-05-09 23:32:25 +0800

    ddns-go

--- immo_diy.sh
+++ immo_diy.sh
@@ -77,8 +77,8 @@ svn_export "master" "utils/cgroupfs-mount" "feeds/packages/utils/cgroupfs-mount"
 svn_export "master" "net/cloudflared" "feeds/packages/net/cloudflared" "https://github.com/openwrt/packages"
 svn_export "main" "easytier" "package/easytier" "https://github.com/EasyTier/luci-app-easytier"
 svn_export "main" "luci-app-easytier" "package/luci-app-easytier" "https://github.com/EasyTier/luci-app-easytier"
-svn_export "master" "luci-app-ddns-go" "package/luci-app-ddns-go" "https://github.com/sirpdboy/luci-app-ddns-go"
-svn_export "master" "ddns-go" "package/ddns-go" "https://github.com/sirpdboy/ddns-go"
+svn_export "master" "luci-app-ddns-go" "package/luci-app-ddns-go" "https://github.com/OldCoding/luci-app-ddns-go"
+svn_export "master" "ddns-go" "package/ddns-go" "https://github.com/OldCoding/ddns-go"
 
 sed -i "/mediaurlbase/d" package/luci-theme-design/root/etc/uci-defaults/30_luci-theme-design
 mv ./package/netspeedtest/* ./package/ && rm -rf ./package/netspeedtest
