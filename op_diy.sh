d4694b6353f6afe94d6a718e7727c1a381782d23
OldCoding <qq1049066154@gmail.com>
2026-05-16 13:15:00 +0800

    test

--- op_diy.sh
+++ op_diy.sh
@@ -62,8 +62,6 @@ rm -rf feeds/packages/utils/runc
 rm -rf feeds/packages/libs/libtorrent-rasterbar
 rm -rf feeds/luci/themes/luci-theme-design
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-rm -rf feeds/packages/libs/icu
-rm -rf feeds/packages/net/samba4
 git clone --depth 1 https://github.com/sbwml/feeds_packages_net_aria2 feeds/packages/net/aria2
 curl -sfL https://github.com/immortalwrt/luci/raw/master/modules/luci-base/root/usr/share/luci/menu.d/luci-base.json > feeds/luci/modules/luci-base/root/usr/share/luci/menu.d/luci-base.json
 #git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
