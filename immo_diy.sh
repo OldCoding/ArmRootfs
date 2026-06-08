b049257864a47bac4b43ebaf005e8cce0334d170
OldCoding <85047508+OldCoding@users.noreply.github.com>
2026-06-08 23:47:02 +0800

    test

--- immo_diy.sh
+++ immo_diy.sh
@@ -40,8 +40,8 @@ svn_export() {
     fi
 }
 
-git clone --depth 1 https://github.com/OldCoding/luci-app-qBittorrent-enhanced.git package/luci-app-qBittorrent-enhanced
-git clone --depth 1 https://github.com/OldCoding/qBEE.git package/qBittorrent-EE
+git clone --depth 1 https://github.com/OldCoding/OpenWrt-qBittorrent-Enhanced-Edition package/qbee
+mv package/qbee package && rm -rf package/qbee
 
 sed -i "s/192.168.1.1/192.168.2.1/g" package/base-files/files/bin/config_generate
 # 安装插件
